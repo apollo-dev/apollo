f26fc44f2a9ee3f375a02effb4d1adfe163f2361
NicholasPiano <nicholas.d.piano@gmail.com>
2015-10-27 17:51:46 +0000

    Adding states and button bindings

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,39 +42,49 @@ $(document).ready(function() {
 	///////////////	ES DEFINITIONS AND MODIFICATIONS
 	// ES
 	experimentSidebar = new Element('experiment-sidebar', SIDEBAR_TEMPLATE);
+	experimentSidebar.states[HOME_STATE] = {'css':'0px'};
+	experimentSidebar.states[NEW_EXPERIMENT_STATE] = defaultState;
 
 	// ES Top Spacer
 	ESTopSpacer = new Element('es-ts', SPACER_TEMPLATE);
 
 	// ES In Progress Button
 	ESInProgressButton = new Element('es-in-progress-button', BUTTON_TEMPLATE);
+	ESInProgressButton.html = 'In progress';
 
 	// ES Settings Button
 	ESSettingsButton = new Element('es-settings-button', BUTTON_TEMPLATE);
+	ESSettingsButton.html = 'settings';
 
 	// ES New Experiment Button
 	ESNewExperimentButton = new Element('es-new-experiment-button', BUTTON_TEMPLATE);
+	ESNewExperimentButton.html = 'New experiment';
 
 	// ES Middle Spacer
 	ESMiddleSpacer = new Element('es-ms', SPACER_TEMPLATE);
 
 	// ES Active Experiments Content Button
 	ESActiveExperimentsContentButton = new Element('es-active-experiments-content-button', BUTTON_TEMPLATE);
+	// ESActiveExperimentsContentButton.classes = ['notouch'];
+	ESActiveExperimentsContentButton.html = 'Active experiments';
 
 	// ES Tray Container
 	ESTrayContainer = new Element('es-tray-container', CONTAINER_TEMPLATE);
 
 	/////////////// ES RENDER
-	// experimentSidebar.render(body);
-	// experimentSidebar.renderChild(ESTopSpacer);
-	// experimentSidebar.renderChild(ESInProgressButton);
-	// experimentSidebar.renderChild(ESSettingsButton);
-	// experimentSidebar.renderChild(ESNewExperimentButton);
-	// experimentSidebar.renderChild(ESMiddleSpacer);
-	// experimentSidebar.renderChild(ESActiveExperimentsContentButton);
-	// experimentSidebar.renderChild(ESTrayContainer);
+	experimentSidebar.render(body);
+	experimentSidebar.renderChild(ESTopSpacer);
+	experimentSidebar.renderChild(ESInProgressButton);
+	experimentSidebar.renderChild(ESSettingsButton);
+	experimentSidebar.renderChild(ESNewExperimentButton);
+	experimentSidebar.renderChild(ESMiddleSpacer);
+	experimentSidebar.renderChild(ESActiveExperimentsContentButton);
+	experimentSidebar.renderChild(ESTrayContainer);
 
 	///////////////	ES BUTTON BINDINGS
+	ESNewExperimentButton.click(function (model) {
+		changeState(model.id, NEW_EXPERIMENT_STATE, {});
+	});
 
 	///////////////////////////////////
 	/////////////// NEW EXPERIMENT SIDEBAR
@@ -105,7 +115,12 @@ $(document).ready(function() {
 	///////////////	NES DEFINITIONS AND MODIFICATIONS
 	// NES
 	newExperimentSidebar = new Element('new-experiment-sidebar', SIDEBAR_TEMPLATE);
-	newExperimentSidebar.specificStyle = {'left':'50px'};
+	newExperimentSidebar.specificStyle = defaultState['css'];
+	newExperimentSidebar.states[HOME_STATE] = defaultState;
+	newExperimentSidebar.states[NEW_EXPERIMENT_STATE] = {
+		'css':{'left':'50px'},
+		'time':defaultAnimationTime
+	}
 
 	// NES Top Spacer
 	NESTopSpacer = new Element('nes-ts', SPACER_TEMPLATE);
@@ -142,18 +157,18 @@ $(document).ready(function() {
 	NESDetailSpacer.classes = ['content'];
 
 	///////////////	NES RENDER
-	// newExperimentSidebar.render(body);
-	// newExperimentSidebar.renderChild(NESTopSpacer);
-	// newExperimentSidebar.renderChild(NESLifPathButton);
-	// newExperimentSidebar.renderChild(NESExperimentNameButton);
-	// newExperimentSidebar.renderChild(NESMiddleSpacer);
-	// newExperimentSidebar.renderChild(NESPreviewButton);
-	// newExperimentSidebar.renderChild(NESBottomSpacer);
-	// newExperimentSidebar.renderChild(NESExperimentCreatedContentButton);
-	// newExperimentSidebar.renderChild(NESExperimentNameContentButton);
-	// newExperimentSidebar.renderChild(NESTrayContainer);
-	// NESTrayContainer.renderChild(NESExtractingExperimentDetailsContentButton);
-	// NESTrayContainer.renderChild(NESDetailSpacer);
+	newExperimentSidebar.render(body);
+	newExperimentSidebar.renderChild(NESTopSpacer);
+	newExperimentSidebar.renderChild(NESLifPathButton);
+	newExperimentSidebar.renderChild(NESExperimentNameButton);
+	newExperimentSidebar.renderChild(NESMiddleSpacer);
+	newExperimentSidebar.renderChild(NESPreviewButton);
+	newExperimentSidebar.renderChild(NESBottomSpacer);
+	newExperimentSidebar.renderChild(NESExperimentCreatedContentButton);
+	newExperimentSidebar.renderChild(NESExperimentNameContentButton);
+	newExperimentSidebar.renderChild(NESTrayContainer);
+	NESTrayContainer.renderChild(NESExtractingExperimentDetailsContentButton);
+	NESTrayContainer.renderChild(NESDetailSpacer);
 
 	///////////////	NES BUTTON BINDINGS
 
@@ -181,7 +196,12 @@ $(document).ready(function() {
 	///////////////	SS DEFINITIONS AND MODIFICATIONS
 	// SS
 	seriesSidebar = new Element('series-sidebar', SIDEBAR_TEMPLATE);
-	seriesSidebar.specificStyle = {'left':'300px'};
+	seriesSidebar.specificStyle = defaultState['css'];
+	seriesSidebar.states[HOME_STATE] = defaultState;
+	seriesSidebar.states[NEW_EXPERIMENT_STATE] = {
+		'css':{'left':'300px'},
+		'time':defaultAnimationTime
+	}
 
 	// SS Top Spacer
 	SSTopSpacer = new Element('ss-ts', SPACER_TEMPLATE);
@@ -193,10 +213,10 @@ $(document).ready(function() {
 	SSPreviewLoadingButton = new Element('ss-preview-loading-button', BUTTON_TEMPLATE);
 
 	///////////////	SS RENDER
-	// seriesSidebar.render(body);
-	// seriesSidebar.renderChild(SSTopSpacer);
-	// seriesSidebar.renderChild(SSTrayContainer);
-	// seriesSidebar.renderChild(SSPreviewLoadingButton);
+	seriesSidebar.render(body);
+	seriesSidebar.renderChild(SSTopSpacer);
+	seriesSidebar.renderChild(SSTrayContainer);
+	seriesSidebar.renderChild(SSPreviewLoadingButton);
 
 	///////////////	SS BUTTON BINDINGS
 
@@ -224,7 +244,12 @@ $(document).ready(function() {
 	///////////////	INFS DEFINITIONS AND MODIFICATIONS
 	// INFS
 	infoSidebar = new Element('info-sidebar', SIDEBAR_TEMPLATE);
-	infoSidebar.specificStyle = {'left':'550px'};
+	infoSidebar.specificStyle = defaultState['css'];
+	infoSidebar.states[HOME_STATE] = defaultState;
+	infoSidebar.states[NEW_EXPERIMENT_STATE] = {
+		'css':{'left':'550px'},
+		'time':defaultAnimationTime
+	}
 
 	// INFS Top Spacer
 	INFSTopSpacer = new Element('infs-ts', SPACER_TEMPLATE);
@@ -246,13 +271,13 @@ $(document).ready(function() {
 	INFSExtractButton = new Element('infs-extract-button', BUTTON_TEMPLATE);
 
 	///////////////	INFS RENDER
-	// infoSidebar.render(body);
-	// infoSidebar.renderChild(INFSTopSpacer);
-	// infoSidebar.renderChild(INFSInfoContentButton);
-	// infoSidebar.renderChild(INFSTrayContainer);
-	// infoSidebar.renderChild(INFSInfoSpacer);
-	// infoSidebar.renderChild(INFSMiddleSpacer);
-	// infoSidebar.renderChild(INFSExtractButton);
+	infoSidebar.render(body);
+	infoSidebar.renderChild(INFSTopSpacer);
+	infoSidebar.renderChild(INFSInfoContentButton);
+	infoSidebar.renderChild(INFSTrayContainer);
+	infoSidebar.renderChild(INFSInfoSpacer);
+	infoSidebar.renderChild(INFSMiddleSpacer);
+	infoSidebar.renderChild(INFSExtractButton);
 
 	///////////////	INFS BUTTON BINDINGS
 
@@ -282,7 +307,7 @@ $(document).ready(function() {
 	///////////////	CES DEFINITIONS AND MODIFICATIONS
 	// CES
 	currentExperimentSidebar = new Element('current-experiment-sidebar', SIDEBAR_TEMPLATE);
-	currentExperimentSidebar.specificStyle = {'left':'50px'};
+	currentExperimentSidebar.specificStyle = defaultState['css'];
 
 	// CES Top Spacer
 	CESTopSpacer = new Element('ces-ts', SPACER_TEMPLATE);
@@ -348,7 +373,7 @@ $(document).ready(function() {
 	///////////////	CS DEFINITIONS AND MODIFICATIONS
 	// CS
 	compositeSidebar = new Element('composite-sidebar', SIDEBAR_TEMPLATE);
-	compositeSidebar.specificStyle = {'left':'300px'};
+	compositeSidebar.specificStyle = defaultState['css'];
 
 	// CS Top Spacer
 	CSTopSpacer = new Element('cs-ts', SPACER_TEMPLATE);
@@ -399,8 +424,8 @@ $(document).ready(function() {
 
 	///////////////	CHS DEFINITIONS AND MODIFICATIONS
 	// CHS
-	channelSidebar = new Element('info-sidebar', SIDEBAR_TEMPLATE);
-	channelSidebar.specificStyle = {'left':'550px'};
+	channelSidebar = new Element('channel-sidebar', SIDEBAR_TEMPLATE);
+	channelSidebar.specificStyle = defaultState['css'];
 
 	// CHS Top Spacer
 	CHSTopSpacer = new Element('chs-ts', SPACER_TEMPLATE);
@@ -459,7 +484,7 @@ $(document).ready(function() {
 	///////////////	DEFINITIONS AND MODIFICATIONS
 	// MODS
 	modSidebar = new Element('mod-sidebar', SIDEBAR_TEMPLATE);
-	modSidebar.specificStyle = {'left':'800px'};
+	modSidebar.specificStyle = defaultState['css'];
 
 	// MODS Top Spacer
 	MODSTopSpacer = new Element('mods-ts', SPACER_TEMPLATE);
@@ -510,18 +535,39 @@ $(document).ready(function() {
 	//        |/         |/   \__/  (_______)  (_______)      \_______)
 	//
 
-	///////////////	ELEMENT VARS
+	///////////////	PROGS ELEMENT VARS
 	var progressSidebar;
 	var PROGSTopSpacer;
 	var PROGSInProgressContentButton;
-	var PROGS
-	var PROGS
-	var PROGS
-	var PROGS
+	var PROGSMiddleSpacer;
+	var PROGSInProgressContainer;
+	var PROGSInProgressButtonDictionary = {};
 
-	///////////////	DEFINITIONS AND MODIFICATIONS
-	///////////////	RENDER
-	///////////////	BUTTON BINDINGS
+	///////////////	PROGS DEFINITIONS AND MODIFICATIONS
+	// PROGS
+	progressSidebar = new Element('progress-sidebar', SIDEBAR_TEMPLATE);
+	progressSidebar.specificStyle = defaultState['css'];
+
+	// PROGS Top Spacer
+	PROGSTopSpacer = new Element('progs-ts', SPACER_TEMPLATE);
+
+	// PROGS In Progress Content Button
+	PROGSInProgressContentButton = new Element('progs-in-progress-content-button', BUTTON_TEMPLATE);
+
+	// PROGS Middle Spacer
+	PROGSMiddleSpacer = new Element('progs-ms', SPACER_TEMPLATE);
+
+	// PROGS In Progess Container
+	PROGSInProgressContainer = new Element('progs-in-progress-container', CONTAINER_TEMPLATE);
+
+	///////////////	PROGS RENDER
+	progressSidebar.render(body);
+	progressSidebar.renderChild(PROGSTopSpacer);
+	progressSidebar.renderChild(PROGSInProgressContentButton);
+	progressSidebar.renderChild(PROGSMiddleSpacer);
+	progressSidebar.renderChild(PROGSInProgressContainer);
+
+	///////////////	PROGS BUTTON BINDINGS
 
 	///////////////////////////////////
 	/////////////// SETTINGS SIDEBAR
@@ -535,9 +581,38 @@ $(document).ready(function() {
 	//        \_______)  (_______/     )_(         \_______)
 	//
 
-	///////////////	ELEMENT VARS
-	///////////////	DEFINITIONS AND MODIFICATIONS
-	///////////////	RENDER
+	///////////////	SETS ELEMENT VARS
+	var settingsSidebar;
+	var SETSTopSpacer;
+	var SETSInProgressContentButton;
+	var SETSMiddleSpacer;
+	var SETSInProgressContainer;
+	var SETSInProgressButtonDictionary = {};
+
+	///////////////	SETS DEFINITIONS AND MODIFICATIONS
+	// SETS
+	settingsSidebar = new Element('settings-sidebar', SIDEBAR_TEMPLATE);
+	settingsSidebar.specificStyle = defaultState['css'];
+
+	// SETS Top Spacer
+	SETSTopSpacer = new Element('sets-ts', SPACER_TEMPLATE);
+
+	// SETS In Progress Content Button
+	SETSInProgressContentButton = new Element('sets-in-progress-content-button', BUTTON_TEMPLATE);
+
+	// SETS Middle Spacer
+	SETSMiddleSpacer = new Element('sets-ms', SPACER_TEMPLATE);
+
+	// SETS In Progess Container
+	SETSInProgressContainer = new Element('sets-in-progress-container', CONTAINER_TEMPLATE);
+
+	///////////////	PROGS RENDER
+	settingsSidebar.render(body);
+	settingsSidebar.renderChild(SETSTopSpacer);
+	settingsSidebar.renderChild(SETSInProgressContentButton);
+	settingsSidebar.renderChild(SETSMiddleSpacer);
+	settingsSidebar.renderChild(SETSInProgressContainer);
+
 	///////////////	BUTTON BINDINGS
 
 	///////////////////////////////////
@@ -564,6 +639,7 @@ $(document).ready(function() {
 	// MS
 	miniSidebar = new Element('mini-sidebar', SIDEBAR_TEMPLATE);
 	miniSidebar.classes = ['mini'];
+	miniSidebar.specificStyle = defaultState['css'];
 
 	// MS Top Spacer
 	MSTopSpacer = new Element('ms-ts', SPACER_TEMPLATE);
