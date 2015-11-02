7428d92f5d01df7c9ab74dccae629ba2539cf14d
NicholasPiano <nicholas.d.piano@gmail.com>
2015-11-02 18:32:28 +0000

    trying to integrate paper, merging with master first

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 	// require stuff
-	var settings = require("./settings.json"); // external settings
+	window.settings = require("./settings.json"); // external settings
 
 	// define window globals for paper to latch on to
 	window.paperMarkerScope = {canvas:'paper-marker-canvas'};
 	window.paperRegionScope = {canvas:'paper-region-canvas'};
 	window.paperSliceScope = {canvas:'paper-slice-canvas'};
 	window.paperLassoScope = {canvas:'paper-lasso-canvas'};
+
+	// metrics
+	window.imageHeight = 512;
+	window.imageWidth = 1024;
 
 });
