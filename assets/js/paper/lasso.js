51bda075ae56d232c8cc2ba8afa6cfdd3720e904
NicholasPiano <nicholas.d.piano@gmail.com>
2015-11-03 18:40:52 +0000

    leaving paper for now. Finishing import.

--- assets/js/paper/lasso.js
+++ assets/js/paper/lasso.js
@@ -3,8 +3,14 @@ var scope = this;
 var view = this.project.view;
 window.paperLassoScope['scope'] = scope;
 
+// needs to register functions in the global vars that can be called from the main script.
+window.paperLassoScope['some_function'] = function (somevars) {
+	// var path...
+	// do something with path.
+}
+
 function onMouseDown(event) {
-	
+
 }
 
 function onMouseDrag(event) {
