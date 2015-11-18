c4be64988c03762c3f05d6e9844b8988cebb6fdc
NicholasPiano <nicholas.d.piano@gmail.com>
2015-11-18 16:45:39 +0000

    going to remove backendz shenanigans
    Can be done simply by saving objects with progress indicators and other
    such variables.

--- img/woot/apps/expt/models.py
+++ img/woot/apps/expt/models.py
@@ -42,6 +42,7 @@ class Experiment(models.Model):
 	def __str__(self):
 		return self.name
 
+	# setup utility
 	def make_paths(self):
 		self.img_path = join(dirname(self.lif_path), self.name)
 		self.source_path = join(self.img_path, 'source')
@@ -60,11 +61,65 @@ class Experiment(models.Model):
 			self.templates.get_or_create(name=name, rx=template['rx'], rv=template['rv'])
 		self.save()
 
-	def list_series(self):
-		return self.lif.series_list()
+	# metadata requests
+	# these commands assume the metadata has already been extracted
+	def series_list(self):
+		series_list = []
+		with open(self.partial_inf_path) as inf:
+			series_list = re.findall('Series #([0-9]+)', inf.read())
 
-	def make_preview_images(self):
-		self.lif.preview_images()
+		return series_list
+
+	def series_metadata(self, series_name):
+		# name
+		# rs, cs, zs, ts, channel names
+		# rmop, cmop, zmop, tpf
+		# any titles or any other relevant information
+		series_metadata = {}
+
+		with open(self.inf_path) as inf:
+			content = inf.read()
+			series_block = block(content, 'Image:{}'.format(series_name), '</Image>')
+
+			# 1. title
+			series_metadata['title'] = re.findall(r'Name="(.+)"', series_block)[0]
+
+			# 2. acquisition date
+			series_metadata['acquisition_date'] = re.findall(r'<AcquisitionDate>(.+)</AcquisitionDate>', series_block)[0]
+
+			# 2. rs, cs, zs, ts, rmop, cmop, zmop
+			pixels_line = block(series_block, '<Pixels', '>')
+			pixels_line_template = r'^<.+PhysicalSizeX="(?P<cmop>.+)" PhysicalSizeY="(?P<rmop>.+)" PhysicalSizeZ="(?P<zmop>.+)" SignificantBits=".+" SizeC=".+" SizeT="(?P<ts>.+)" SizeX="(?P<cs>.+)" SizeY="(?P<rs>.+)" SizeZ="(?P<zs>.+)" Type=".+"$'
+			pixels_line_template_no_Z = r'^<.+PhysicalSizeX="(?P<cmop>.+)" PhysicalSizeY="(?P<rmop>.+)" SignificantBits=".+" SizeC=".+" SizeT="(?P<ts>.+)" SizeX="(?P<cs>.+)" SizeY="(?P<rs>.+)" SizeZ="(?P<zs>.+)" Type=".+"$'
+
+			if re.match(pixels_line_template, pixels_line) is not None:
+				match_dict = re.match(pixels_line_template, pixels_line).groupdict()
+			else:
+				match_dict = re.match(pixels_line_template_no_Z, pixels_line).groupdict()
+
+			series_metadata.update(match_dict)
+
+			# 3. tpf
+			tpf_in_seconds = 0
+			preview_image_index = 0
+			line_template = r'^<Plane DeltaT="(?P<delta_t>.+)" PositionX=".+" PositionY=".+" PositionZ=".+" TheC="(?P<c>.+)" TheT="(?P<t>.+)" TheZ="(?P<z>.+)"/>$'
+			lines = [l for l in series_block.split('\n') if 'Plane DeltaT' in l]
+			for line in lines:
+				line_dict = re.match(line_template, line.strip()).groupdict()
+				if (line_dict['c'], line_dict['t'], line_dict['z']) == ('0','1','0'):
+					tpf_in_seconds = float(line_dict['delta_t'])
+
+				if (line_dict['c'], line_dict['t'], line_dict['z']) == ('1','0', str(int(float(series_metadata['zs']) / 2.0))):
+					# get number of occurences of the string "<Plane" before this point in the whole file
+					previous_block = block(series_block, 'Pixels', line)
+					preview_image_index = len(re.findall(r'<Plane', previous_block))
+
+			series_metadata['tpf_in_seconds'] = tpf_in_seconds
+			series_metadata['preview_image_index'] = preview_image_index
+
+			# 4. channel names
+			channel_names = re.findall(r'Channel:.+:(.+)" N', series_block)
+			series_metadata['channel_names'] = channel_names
 
 class Series(models.Model):
 	# connections
@@ -118,7 +173,7 @@ class Series(models.Model):
 	def metadata(self):
 		if not self.metadata_set:
 			self.metadata_set = True
-			metadata = self.experiment.lif.series_metadata(self.name)
+			metadata = self.experiment.series_metadata(self.name)
 
 			self.title = metadata['title']
 			self.acquisition_date = parse_datetime(metadata['acquisition_date'])
@@ -206,164 +261,3 @@ class Path(models.Model):
 
 	def url(self):
 		return join(self.root, self.file_name)
-
-class LifFile(models.Model):
-	# connections
-	experiment = models.OneToOneField(Experiment, related_name='lif')
-
-	# properties
-	partial_metadata_extracted = models.BooleanField(default=False)
-	metadata_extracted = models.BooleanField(default=False)
-
-	# methods
-	def partial_metadata(self):
-		# omit the "-omexml" to only get some of the metadata
-		if not self.partial_metadata_extracted:
-			if not exists(self.experiment.partial_inf_path):
-				extract_partial_metadata.delay(self.experiment.lif_path, self.experiment.partial_inf_path)
-			self.partial_metadata_extracted = True
-			self.save()
-
-		return open(self.experiment.partial_inf_path, 'r')
-
-	def metadata(self):
-		if not self.metadata_extracted:
-			if not exists(self.experiment.inf_path):
-				showinf = join(settings.BIN_ROOT, 'bftools', 'showinf')
-				call('{} -no-upgrade -novalid -nopix -omexml {} > {}'.format(showinf, self.experiment.lif_path, self.experiment.inf_path), shell=True)
-			self.metadata_extracted = True
-			self.save()
-
-		return open(self.experiment.inf_path, 'r')
-
-	def series_list(self):
-		series = []
-		with self.partial_metadata() as inf:
-			series = re.findall('Series #([0-9]+)', inf.read())
-
-		return series
-
-	def series_metadata(self, series_name):
-		# name
-		# rs, cs, zs, ts, channel names
-		# rmop, cmop, zmop, tpf
-		# any titles or any other relevant information
-		series_metadata = {}
-
-		with self.metadata() as inf:
-			content = inf.read()
-			series_block = block(content, 'Image:{}'.format(series_name), '</Image>')
-
-			# 1. title
-			series_metadata['title'] = re.findall(r'Name="(.+)"', series_block)[0]
-
-			# 2. acquisition date
-			series_metadata['acquisition_date'] = re.findall(r'<AcquisitionDate>(.+)</AcquisitionDate>', series_block)[0]
-
-			# 2. rs, cs, zs, ts, rmop, cmop, zmop
-			pixels_line = block(series_block, '<Pixels', '>')
-			pixels_line_template = r'^<.+PhysicalSizeX="(?P<cmop>.+)" PhysicalSizeY="(?P<rmop>.+)" PhysicalSizeZ="(?P<zmop>.+)" SignificantBits=".+" SizeC=".+" SizeT="(?P<ts>.+)" SizeX="(?P<cs>.+)" SizeY="(?P<rs>.+)" SizeZ="(?P<zs>.+)" Type=".+"$'
-			pixels_line_template_no_Z = r'^<.+PhysicalSizeX="(?P<cmop>.+)" PhysicalSizeY="(?P<rmop>.+)" SignificantBits=".+" SizeC=".+" SizeT="(?P<ts>.+)" SizeX="(?P<cs>.+)" SizeY="(?P<rs>.+)" SizeZ="(?P<zs>.+)" Type=".+"$'
-
-			if re.match(pixels_line_template, pixels_line) is not None:
-				match_dict = re.match(pixels_line_template, pixels_line).groupdict()
-			else:
-				match_dict = re.match(pixels_line_template_no_Z, pixels_line).groupdict()
-
-			series_metadata.update(match_dict)
-
-			# 3. tpf
-			tpf_in_seconds = 0
-			preview_image_index = 0
-			line_template = r'^<Plane DeltaT="(?P<delta_t>.+)" PositionX=".+" PositionY=".+" PositionZ=".+" TheC="(?P<c>.+)" TheT="(?P<t>.+)" TheZ="(?P<z>.+)"/>$'
-			lines = [l for l in series_block.split('\n') if 'Plane DeltaT' in l]
-			for line in lines:
-				line_dict = re.match(line_template, line.strip()).groupdict()
-				if (line_dict['c'], line_dict['t'], line_dict['z']) == ('0','1','0'):
-					tpf_in_seconds = float(line_dict['delta_t'])
-
-				if (line_dict['c'], line_dict['t'], line_dict['z']) == ('1','0', str(int(float(series_metadata['zs']) / 2.0))):
-					# get number of occurences of the string "<Plane" before this point in the whole file
-					previous_block = block(series_block, 'Pixels', line)
-					preview_image_index = len(re.findall(r'<Plane', previous_block))
-
-			series_metadata['tpf_in_seconds'] = tpf_in_seconds
-			series_metadata['preview_image_index'] = preview_image_index
-
-			# 4. channel names
-			channel_names = re.findall(r'Channel:.+:(.+)" N', series_block)
-			series_metadata['channel_names'] = channel_names
-
-		return series_metadata
-
-	def preview_images(self):
-		# this unfortunately has to been done in one lump for speed and consistency purposes.
-		# The preview image for the processed series (composite) can be done later with more
-		# information.
-
-		# 1. get range of images from each series
-		# - I have to get all preview images from the series in the same call because the JVM starts and stops every time
-		# it is run, which is annoying. This means I have extract using a call that will not throw an error with any
-		# series. This mean I can't use "timepoint", "z", "channel" as series might not contain these. If I use "range",
-		# it will not throw an error if the range is larger. So, I have decided to use half the maximum z of any
-		# series for the upper limit. Then, each series will be in a separate tiff and can be separated again.
-		if len(os.listdir(self.experiment.preview_path)) < self.experiment.series.count():
-			max_z = max([series.zs for series in self.experiment.series.all()]) + 1
-
-			bfconvert = join(settings.BIN_ROOT, 'bftools', 'bfconvert')
-			fake_preview_path = join(self.experiment.preview_path, '{}_s%s_ch%c_t%t_z%z_preview.tiff'.format(self.experiment.name))
-			call('{bf} -range 0 {max_z} {path} {out}'.format(bf=bfconvert, max_z=max_z, path=self.experiment.lif_path, out=fake_preview_path), shell=True)
-
-		# 2. convert tiff to png for browser
-		for series in self.experiment.series.all():
-			series.preview_path = join(self.experiment.preview_path, '{}_s{}_ch{}_t{}_z{}_preview.png'.format(self.experiment.name, series.name, series.max_channel(), 0, series.mid_z()))
-			series.save()
-
-			# select preview path using max_channel and mid_z
-			if not exists(series.preview_path):
-				selected_path = join(self.experiment.preview_path, '{}_s{}_ch{}_t{}_z{}_preview.tiff'.format(self.experiment.name, series.name, series.max_channel(), 0, series.mid_z()))
-				convert = join(settings.BIN_ROOT, 'convert')
-				call('{} -contrast-stretch 0 {} {}'.format(convert, selected_path, series.preview_path), shell=True)
-
-		# delete everything that is not the selected paths
-		for file_name in os.listdir(self.experiment.preview_path):
-			if join(self.experiment.preview_path, file_name) not in [series.preview_path for series in self.experiment.series.all()]:
-				os.remove(join(self.experiment.preview_path, file_name))
-
-	def extract_series(self, series_name):
-		# get series
-		series = self.experiment.series.get(name=series_name)
-
-		# wait in queue
-		extraction_counter = self.experiment.extraction_counter
-		while extraction_counter == self.experiment.extraction_cap:
-			# reset from db (is this the right way?)
-			extraction_counter = self.experiment.extraction_counter
-
-			# set "in-queue"
-			series.in_queue = True
-			series.save()
-
-		# when out of queue
-		series.in_queue = False
-		series.is_new = False
-		series.save()
-
-		# extract
-		source_path = join(self.experiment.source_path, '{}_s{}_ch-%c_t%t_z%z.tiff'.format(self.experiment.name, series_name))
-		lif_path = self.experiment.lif_path
-		bfconvert = join(settings.BIN_ROOT, 'bftools', 'bfconvert')
-		cmd = '{} -series {} {} {}'.format(bfconvert, series_name, lif_path, source_path)
-
-		with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as extract_stream:
-			ln_template = r'.+Converted .+/.+ planes \((?P<percentage>.+)%\)'
-			for ln in extract_stream.stderr:
-				if 'Converted' in ln:
-					ln_match = re.match(ln_template, ln)
-					percentage = int(ln_match.group('percentage'))
-
-					series.source_extraction_percentage = percentage
-					series.save()
-
-		series.processing_complete = True
-		series.save()
--- img/woot/apps/expt/tasks.py
+++ img/woot/apps/expt/tasks.py
@@ -6,6 +6,7 @@ from django.conf import settings
 # local
 from woot.celery import apollo_celery_app
 from apps.expt import backend
+from apps.expt.models import Experiment, Series
 
 # util
 # from celery import Task
@@ -21,28 +22,100 @@ def extract_partial_metadata(self, lif_path, partial_inf_path):
 	showinf = join(settings.BIN_ROOT, 'bftools', 'showinf')
 	line_template = r'.+Converted .+/.+ planes \((?P<percentage>.+)%\)'
 	with Popen('{} -no-upgrade -novalid -nopix {} > {}'.format(showinf, lif_path, partial_inf_path)) as extract_partial_metadata_proc:
-		for line in extract_partial_metadata_proc.stderr:
-			status = 'starting'
-			if 'Converted' in line:
-				# get progress from output
-				status = 'converting'
+		status = 'extracting'
+		current = 0
 
-				line_match = re.match(line_template, line)
-				current = int(ln_match.group('percentage'))
+		# update progress
+		backend.set_progress(self.request.id, current, status)
 
-			# update progress
-			backend.set_progress(self.request.id, current, status)
-
-	return self.request.id
+	return 'done'
 
 @apollo_celery_app.task(bind=True)
-def extract_metadata(self):
-	pass
+def extract_metadata(self, lif_path, inf_path):
+	showinf = join(settings.BIN_ROOT, 'bftools', 'showinf')
+	line_template = r'.+Converted .+/.+ planes \((?P<percentage>.+)%\)'
+	with Popen('{} -no-upgrade -novalid -nopix -omexml {} > {}'.format(showinf, lif_path, inf_path)) as extract_metadata_proc:
+		status = 'extracting'
+		current = 0
+
+		# update progress
+		backend.set_progress(self.request.id, current, status)
+
+	return 'done'
 
 @apollo_celery_app.task(bind=True)
-def extract_preview_images(self):
-	pass
+def extract_preview_images(self, experiment_pk):
+	# this unfortunately has to been done in one lump for speed and consistency purposes.
+	# The preview image for the processed series (composite) can be done later with more
+	# information.
+
+	experiment = Experiment.objects.get(pk=experiment_pk)
+
+	# 1. get range of images from each series
+	# - I have to get all preview images from the series in the same call because the JVM starts and stops every time
+	# it is run, which is annoying. This means I have extract using a call that will not throw an error with any
+	# series. This mean I can't use "timepoint", "z", "channel" as series might not contain these. If I use "range",
+	# it will not throw an error if the range is larger. So, I have decided to use half the maximum z of any
+	# series for the upper limit. Then, each series will be in a separate tiff and can be separated again.
+	if len(os.listdir(experiment.preview_path)) < experiment.series.count():
+		max_z = max([series.zs for series in experiment.series.all()]) + 1
+
+		bfconvert = join(settings.BIN_ROOT, 'bftools', 'bfconvert')
+		fake_preview_path = join(experiment.preview_path, '{}_s%s_ch%c_t%t_z%z_preview.tiff'.format(experiment.name))
+		call('{bf} -range 0 {max_z} {path} {out}'.format(bf=bfconvert, max_z=max_z, path=experiment.lif_path, out=fake_preview_path), shell=True)
+
+	# 2. convert tiff to png for browser
+	for series in experiment.series.all():
+		series.preview_path = join(experiment.preview_path, '{}_s{}_ch{}_t{}_z{}_preview.png'.format(experiment.name, series.name, series.max_channel(), 0, series.mid_z()))
+		series.save()
+
+		# select preview path using max_channel and mid_z
+		if not exists(series.preview_path):
+			selected_path = join(experiment.preview_path, '{}_s{}_ch{}_t{}_z{}_preview.tiff'.format(experiment.name, series.name, series.max_channel(), 0, series.mid_z()))
+			convert = join(settings.BIN_ROOT, 'convert')
+			call('{} -contrast-stretch 0 {} {}'.format(convert, selected_path, series.preview_path), shell=True)
+
+	# delete everything that is not the selected paths
+	for file_name in os.listdir(experiment.preview_path):
+		if join(experiment.preview_path, file_name) not in [series.preview_path for series in experiment.series.all()]:
+			os.remove(join(experiment.preview_path, file_name))
 
 @apollo_celery_app.task(bind=True)
-def extract_series(self):
-	pass
+def extract_series(self, experiment_pk, series_name):
+	# get series
+	experiment = Experiment.objects.get(pk=experiment_pk)
+	series = experiment.series.get(name=series_name)
+
+	# wait in queue
+	extraction_counter = experiment.extraction_counter
+	while extraction_counter == experiment.extraction_cap:
+		# reset from db (is this the right way?)
+		extraction_counter = experiment.extraction_counter
+
+		# set "in-queue"
+		series.in_queue = True
+		series.save()
+
+	# when out of queue
+	series.in_queue = False
+	series.is_new = False
+	series.save()
+
+	# extract
+	source_path = join(experiment.source_path, '{}_s{}_ch-%c_t%t_z%z.tiff'.format(experiment.name, series_name))
+	lif_path = experiment.lif_path
+	bfconvert = join(settings.BIN_ROOT, 'bftools', 'bfconvert')
+	cmd = '{} -series {} {} {}'.format(bfconvert, series_name, lif_path, source_path)
+
+	with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as extract_stream:
+		ln_template = r'.+Converted .+/.+ planes \((?P<percentage>.+)%\)'
+		for ln in extract_stream.stderr:
+			if 'Converted' in ln:
+				ln_match = re.match(ln_template, ln)
+				percentage = int(ln_match.group('percentage'))
+
+				series.source_extraction_percentage = percentage
+				series.save()
+
+	series.processing_complete = True
+	series.save()
